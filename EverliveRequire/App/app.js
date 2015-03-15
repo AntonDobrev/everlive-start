4ab8efbb951b96f36b383f1abfbcec1cb08f8586
AntonDobrev <AntonDobrev@users.noreply.github.com>
2015-03-15 18:49:48 +0200

    Update app.js

--- EverliveRequire/App/app.js
+++ EverliveRequire/App/app.js
@@ -6,26 +6,24 @@ define([
 
            var app = {
                
-               init: function () {
-                   var self = this;
-                   
-                                      alert("Initializing");
-                   self.everlive = new Everlive({
-                                                    apiKey: config.everlive.apiKey,
-                                                    scheme: config.everlive.scheme
-                                                });
-            
-                   self.mobileApp = new kendo.mobile.Application(document.body, {
-                                                                     skin: 'flat'
-                                                                 });
-                 
-                   self.everlive.data("Appraisals").get(null, function () {
-                   	alert('success');
-                   }, function () {
-                   	alert('err');
-                   });
-
-               }
+                      init: function () {
+                                 var self = this;
+                                 
+                                 self.everlive = new Everlive({
+                                            apiKey: config.everlive.apiKey,
+                                            scheme: config.everlive.scheme
+                                 });
+                                 
+                                 self.mobileApp = new kendo.mobile.Application(document.body, {
+                                                                                skin: 'flat'
+                                                                            });
+                                 
+                                 self.everlive.data("Appraisals").get(null, function () {
+                                                       alert('success');
+                                            }, function () {
+                                                       alert('err');
+                                            });
+                      }
            };
            return app;
        });
